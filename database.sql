b660b017816eca3d2d31d311af05de14e54b5746
alexpoe22 <alexpoe22@gmail.com>
2017-03-15 15:20:08 -0700

    term transactions

--- database.sql
+++ database.sql
@@ -18,8 +18,9 @@ CREATE TABLE CheckTerm (
 
   CREATE TABLE TermTransactions (
       ID int,
-      TermID,
-      Twenty int,
-      Thirty int,
-      Fifty int,
+      CheckTermID int FOREIGN KEY REFERENCES CheckTerm(ID),
+      Transaction int,
+      Description varchar(255),
+      Photo varchar(255),
+      PRIMARY KEY (ID)
     );
